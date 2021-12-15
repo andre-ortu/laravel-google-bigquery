fba851cc6755efdba56acc2b470acc3a68be06b4
andre-ortu <andrea.ortu.9229@gmail.com>
2021-12-15 11:48:35 +0100

    Add Config Variables

--- src/config/google_bigquery.php
+++ src/config/google_bigquery.php
@@ -2,4 +2,6 @@
 
 return [
     'credentials' => env('GOOGLE_BIGQUERY_CREDENTIALS'),
+    'projectId' => env('GOOGLE_BIG_QUERY_PROJECT_ID'),
+    'datasetId' => env('GOOGLE_BIG_QUERY_DATASET_ID'),
 ];
